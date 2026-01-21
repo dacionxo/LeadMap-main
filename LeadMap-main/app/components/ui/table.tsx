ee59a813874fa919138303126b4b9ab51e2263d0
dacionxo <david@growyourdigitalleverage.com>
2026-01-21 00:23:31 -0500

    Apply dense table styling: reduce padding to p-2 for more compact rows, matching TailwindAdmin dense table design

--- LeadMap-main/app/components/ui/table.tsx
+++ LeadMap-main/app/components/ui/table.tsx
@@ -76,7 +76,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      'h-12 px-4 text-left align-middle text-base font-semibold text-ld [&:has([role=checkbox])]:pr-0',
+      'p-2 text-left align-middle text-base font-semibold text-ld [&:has([role=checkbox])]:pr-0',
       className
     )}
     {...props}
@@ -90,7 +90,7 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn('p-4 align-middle [&:has([role=checkbox])]:pr-0', className)}
+    className={cn('p-2 align-middle [&:has([role=checkbox])]:pr-0', className)}
     {...props}
   />
 ))
